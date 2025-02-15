9f7a862c9ada2d1fcede6e5507dc9761d92726cc
lalosianturi21 <fulalotio@gmail.com>
2025-02-15 21:36:00 +0700

    update new

--- src/pages/home/container/Skills.jsx
+++ src/pages/home/container/Skills.jsx
@@ -14,48 +14,39 @@ const Skills = () => {
                             <i className='bx bxl-html5 skills__icon'></i>
                             <span className="skills__name">HTML5</span>
                         </div>
-                        <div className="skills__bar skills__html">
+                        <div className="skills__bar skills__project">
 
                         </div>
-                        <div>
-                            <span className="skills__percentage">95%</span>
-                        </div>
+                      
                     </div>
                     <div className="skills__data">
                         <div className="skills__names">
                             <i className='bx bxl-css3 skills__icon'></i>
                             <span className="skills__name">CSS3</span>
                         </div>
-                        <div className="skills__bar skills__css">
+                        <div className="skills__bar skills__project">
 
                         </div>
-                        <div>
-                            <span className="skills__percentage">85%</span>
-                        </div>
+                       
                     </div>
                     <div className="skills__data">
                         <div className="skills__names">
                             <i className='bx bxl-javascript skills__icon' ></i>
                             <span className="skills__name">JAVASCRIPT</span>
                         </div>
-                        <div className="skills__bar skills__js">
+                        <div className="skills__bar skills__project">
 
                         </div>
-                        <div>
-                            <span className="skills__percentage">65%</span>
-                        </div>
+                       
                     </div>
                     <div className="skills__data">
                         <div className="skills__names">
                             <i className='bx bxs-paint skills__icon'></i>
                             <span className="skills__name">UX/UI</span>
                         </div>
-                        <div className="skills__bar skills__ux">
-
+                        <div className="skills__bar skills__project">
                         </div>
-                        <div>
-                            <span className="skills__percentage">85%</span>
-                        </div>
+                      
                     </div>
                 </div>
                 <div>              
